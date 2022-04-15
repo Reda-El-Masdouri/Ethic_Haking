911520e1114c2951de4f53c71e9e6969c277e0c6
Reda E <reda.elmasdouri@gmail.com>
2022-04-15 22:41:55 +0200

    Amélioration au niveau du script client, reconnexion au serveur

--- création_du_serveur_client_et_connexion.py
+++ création_du_serveur_client_et_connexion.py
@@ -3,23 +3,32 @@
 
 
 import socket
+import time
+
 
 HOST_IP = "127.0.0.1"
 HOST_PORT = 32000
 
 
-s = socket.socket()
+
 print("Connexion au serveur", HOST_IP, ", port", HOST_PORT)
-try:
-    s.connect((HOST_IP, HOST_PORT))                       # Cette fonction est bloquante
-                                                          # Il faut lancer la socket serveur puis celle du client
-                                                          # Si une seule machine est utilisée: ouvrir les deux 
-                                                          # scripts chacun sur une fenêtre.
-except ConnectionRefusedError:
-    print("ERREUR: impossible de se connecter au serveur")
-else:
-    print("Connecté au serveur")
-    s.close()
+while 1:
+    try:
+        s = socket.socket()           # après chaque connexion echoué on crée une nouvelle socket
+        s.connect((HOST_IP, HOST_PORT))                       # Cette fonction est bloquante
+                                                              # Il faut lancer la socket serveur puis celle du client
+                                                              # Si une seule machine est utilisée: ouvrir les deux
+                                                              # scripts chacun sur une fenêtre.
+    except ConnectionRefusedError:
+        print("ERREUR: impossible de se connecter au serveur. Reconnexion ...")
+        time.sleep(4)
+    else:
+        print("Connecté au serveur")
+        break
+
+
+
+s.close()
 
 
 
